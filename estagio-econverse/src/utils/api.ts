19fb0154249328c058a80521576dbe73f8051edd
Gustavo Anjos <162619198+GustavoAnjos2005@users.noreply.github.com>
2024-09-12 14:20:09 -0300

    Proxy

--- estagio-econverse/src/utils/api.ts
+++ estagio-econverse/src/utils/api.ts
@@ -1,11 +1,14 @@
 export const fetchProducts = async () => {
   try {
-    const response = await fetch('https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json');
+    const proxyUrl = 'https://corsproxy.io/?';
+    const targetUrl = 'https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json';
+    const response = await fetch(proxyUrl + encodeURIComponent(targetUrl));
+    
     if (!response.ok) {
       throw new Error('Erro ao buscar os produtos');
     }
     const data = await response.json();
-    return data.products || []; // Retorna a lista de produtos ou um array vazio se não encontrar produtos
+    return data.products || [];
   } catch (error) {
     console.error('Erro ao consumir o JSON:', error);
     return [];
